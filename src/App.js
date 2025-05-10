95b15bfb6776521634f44e2269a5115ba67b1052
starzyck <katsb@wp.pl>
2025-05-10 11:52:45 +0200

    interactions

--- src/App.js
+++ src/App.js
@@ -4,31 +4,43 @@ import {useState} from "react";
 
 function App() {
     const [email, setEmail] = useState('fracz@agh.edu.pl');
-
+    const [ausername,setausername] = useState(null);
     function handleChange(event) {
         setEmail(event.target.value);
     }
 
 
-    let message;
-    if (email.length < 10) {
-        message = <div>Ale masz krótki adres!</div>;
-    } else if (email.length < 15) {
-        message = <div>Twój adres e-mail jest w sam raz.</div>;
-    } else {
-        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
-    }
-
 
     return (
+
+
         <div>
             <h1>System do zapisów na zajęcia</h1>
 
             <h2>Twój e-mail to {email}</h2>
-            <div>{message}</div>
+
+            { !ausername &&(
+
+            <div>
 
             <input type="text" value={email} onChange={handleChange}/>
+            <button type="button" onClick={() => setausername(email)}>
+
+                Wchodzę
+            </button>
+            </div>
+                )}
+
+            {ausername && (
+
+                    <div>
+                        <h4>ZALOGOWANY {email}</h4>
+                    <a onClick={() => setausername(null)}> Wyloguj </a>
+                    </div>
+                )}
+
         </div>
+
     );
 }
 export default App;
